e8c9004a8967d46e2b962a18146e310198d27df1
BigJeffTheChef <marleygpeter@gmail.com>
2022-05-20 17:06:19 +0100

    basic ui for rock paper sissors

--- foundations/project-rock-paper-sissors/rps.js
+++ foundations/project-rock-paper-sissors/rps.js
@@ -4,6 +4,66 @@ const SISSORS = 'sissors';
 const WIN = 'You win!';
 const LOSE = 'You lose!';
 const DRAW = 'It\'s a draw!';
+let scorePlayer = 0;
+let scoreComputer = 0;
+const BESTOF = 5;
+const winningScore = Math.ceil(BESTOF / 2);
+
+const roundResult = document.querySelector('.roundResult');
+const gameResult = document.querySelector('.gameResult');
+
+document.querySelectorAll('.btn').forEach(b => b.addEventListener('click', () => {
+    const result = playRound(b.id, simulateChoice());
+}));
+
+
+
+function playRound(playerChoice, computerChoice) {
+    const player = playerChoice.toLowerCase();
+    const computer = computerChoice.toLowerCase();
+
+    let outcome = '';
+    if (player === ROCK) {
+        if (computer === ROCK) {
+            outcome = DRAW;
+        } else if (computer === PAPER) {
+            outcome = LOSE;
+        } else {
+            outcome = WIN;
+        }
+    } else if (player === PAPER) {
+        if (computer === ROCK) {
+            outcome =  WIN;
+        } else if (computer === PAPER) {
+            outcome =  DRAW;
+        } else {
+            outcome =  LOSE;
+        }
+    } else {
+        if (computer === ROCK) {
+            outcome =  LOSE;
+        } else if (computer === PAPER) {
+            outcome =  WIN;
+        } else {
+            outcome =  DRAW;
+        }
+    }
+    if (outcome === WIN) scorePlayer++;
+    else if (outcome === LOSE) scoreComputer++;
+
+    const roundResultScores = document.querySelector('.scores');
+    roundResultScores.textContent = `Player chose ${playerChoice}, Computer chose ${computerChoice}.`;
+    const roundResultChoices = document.querySelector('.choices');
+    roundResultChoices.textContent = `Scores: Player (${scorePlayer}) - Computer (${scoreComputer})`;
+
+
+    if (scorePlayer >= winningScore || scoreComputer >= winningScore) {
+        const gameResultMsg = document.querySelector('.winMsg');
+        gameResultMsg.textContent = ((scorePlayer >= winningScore) ? 'Player wins!' : 'Computer Wins!') + ' best of ' + BESTOF;
+        scoreComputer = 0;
+        scorePlayer = 0;
+    }
+}
 
 function simulateChoice() {
     let compChoice = Math.floor(Math.random() * 3) + 1;
@@ -23,50 +83,18 @@ function simulateChoice() {
     }
 }
 
-function playRound(playerChoice, computerChoice) {
-    const player = playerChoice.toLowerCase();
-    const computer = computerChoice.toLowerCase();
+// console.group('Test Print');
+// console.log(playRound(ROCK, PAPER));
+// console.log(playRound(ROCK, SISSORS));
+// console.log(playRound(ROCK, ROCK));
+// console.groupEnd();
 
-
-    if (player === ROCK) {
-        if (computer === ROCK) {
-            return DRAW;
-        } else if (computer === PAPER) {
-            return LOSE;
-        } else {
-            return WIN;
-        }
-    } else if (player === PAPER) {
-        if (computer === ROCK) {
-            return WIN;
-        } else if (computer === PAPER) {
-            return DRAW;
-        } else {
-            return LOSE;
-        }
-    } else {
-        if (computer === ROCK) {
-            return LOSE;
-        } else if (computer === PAPER) {
-            return WIN;
-        } else {
-            return DRAW;
-        }
-    }
-}
-
-console.group('Test Print');
-console.log(playRound(ROCK, PAPER));
-console.log(playRound(ROCK, SISSORS));
-console.log(playRound(ROCK, ROCK));
-console.groupEnd();
-
-function game(bestOf = 5) {
+/*function simulateGame(bestOf = 5) {
     if (bestOf % 2 !== 1) {
         console.log('cannot have best of even number');
         return;
     }
-    let scores = [0,0]; /*player index 0, computer index 1*/
+    let scores = [0,0]; /*player index 0, computer index 1
     const winningScore = Math.ceil(bestOf / 2);
     console.log('Best of ' + bestOf + ', so winning score will be: ' + winningScore)
     for (let i = 1; scores[0] < winningScore && scores[1] < winningScore; i++) {
@@ -94,4 +122,4 @@ function game(bestOf = 5) {
         console.log('computer wins!');
     }
     
-}
+}*/
