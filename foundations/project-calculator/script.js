a1fcff1ee2393be50ce7c8a9bf00038032637060
BigJeffTheChef <marleygpeter@gmail.com>
2022-05-24 18:26:39 +0100

    beginning order of operations (PEDMAS)

--- foundations/project-calculator/script.js
+++ foundations/project-calculator/script.js
@@ -5,54 +5,74 @@ let resultOfCalc = 0;
 // capture nodes
 const buttonsNumpad = document.querySelectorAll('.numBtn');
 const buttonsOperations = document.querySelectorAll('.opBtn');
-
 const buttonClear = document.querySelector('.clearBtn');
-const buttonEqual = document.querySelector('.equalBtn');
+const buttonEqual = document.querySelector('#equalBtn');
 
 const displayMain = document.querySelector('.display');
 const displayCalc = document.querySelector('.current-calc');
 
 // add event listers to nodes
 buttonClear.addEventListener('click', () => clear());
-buttonsNumpad.forEach(btn => btn.addEventListener('click', () => clickNumber(btn)));
-buttonsOperations.forEach(btn => btn.addEventListener('click', () => clickOperation(btn)));
+buttonsNumpad.forEach(btn => btn.addEventListener('click', () => clickButton(btn)));
+buttonsOperations.forEach(btn => btn.addEventListener('click', () => clickButton(btn)));
+buttonEqual.addEventListener('click', () => displayMain.value=  arithmetic(displayCalc.value.split('')));
 
 // functions
 
 // 
-function clickNumber(btn) {
-    if (operationComplete) {
-        displayMain.value = btn.textContent;
-        clearArr(operations);
-        displayCalc.value = operations;
-        operationComplete = false;
-    } else {
-        displayMain.value = displayMain.value + btn.textContent;
-    }
-    console.log('display now shows: ' + displayMain.value);
+function clickButton(btn) {
+    displayCalc.value = displayCalc.value + btn.textContent;
 }
 
-function clickOperation(btn) {
-    const operator = btn.textContent;
-    if (operationComplete) operationComplete = false;
-    if (operations[operations.length-1] === '=') {
-        clearArr(operations);
+function collapseBrackets(equation) {
+    // if no brackets, return simple arithmetic on equation
+    if (equation.findIndex(element => element === '(') === -1) {
+        return arithmetic(equation);
     }
-    operations.push(displayMain.value);
-    operations.push(operator);
-    displayCalc.value = operations.join(' ');
-    if (operator === '=') {
-        displayMain.value = computeResult(operations);
-    } else {
-        displayMain.value = '';
+
+    // if there are brackets, recursively solve each bracket, then return the simple arithmetic
+    let parsed = false;
+    while (!parsed) {
+        let nestedParen = 0;
+        let sub = [];
+        let begin = false;
+        let startIndex, endIndex;
+        for (let i = 0; i < equation.length; i++) {
+            if (equation[i] === '(') {
+                nestedParen++;
+                startIndex = i;
+                begin = true;
+            } else if (equation[i] === ')') {
+                nestedParen--;
+            }
+            if (begin) {
+                sub.push(equation[i]);
+            }
+            if (nestedParen === 0 && begin) {
+                begin = false;
+                endIndex = i;
+                let collapsed = collapseBrackets(equation.slice(startIndex+1,endIndex));
+                equation.splice(startIndex, endIndex - startIndex +1, collapsed);
+                i = 0;
+            }
+        }
+        parsed =true;
     }
+    return equation;
 }
 
-function computeResult(opArray) {
+function arithmetic(opArray) {
+
     let result = null;
     let operand = null;
     let operator = null;
-    opArray.forEach(element => {
+
+    if (opArray.lastIndexOf('(') != -1) {
+        opArray = collapseBrackets(opArray);
+    }
+
+    opArray.forEach(e => {
+        let element = e.toString();
         if (element.match(/\d+/)) {
             if (result === null) {
                 result = parseFloat(element);
@@ -79,7 +99,7 @@ function computeResult(opArray) {
                         result = 'cannot divide by zero';
                     } else {
                         result /= operand;
-                    } 
+                    }
                     break;
                 case '^':
                     result = result ** operand;
@@ -90,7 +110,7 @@ function computeResult(opArray) {
             }
             operator = null;
         }
-        
+
     });
     resultOfCalc = result;
     return result;
