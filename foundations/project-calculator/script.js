772ce5ef5e4185d3e4f7402d500fb7ce7f997e6b
BigJeffTheChef <marleygpeter@gmail.com>
2022-05-26 13:04:17 +0100

    basic pedmas, without brackets, implemented

--- foundations/project-calculator/script.js
+++ foundations/project-calculator/script.js
@@ -1,6 +1,3 @@
-let operations = [];
-let operationComplete = false;
-let resultOfCalc = 0;
 const operators = {
     exponent: '^',
     multiplication: '*',
@@ -8,6 +5,7 @@ const operators = {
     addition: '+',
     subtraction: '-',
 }
+let resultDelivered = false;
 
 // // capture nodes
 const buttonsNumpad = document.querySelectorAll('.numBtn');
@@ -20,101 +18,68 @@ const displayCalc = document.querySelector('.current-calc');
 
 // add event listers to nodes
 buttonClear.addEventListener('click', () => clear());
-buttonsNumpad.forEach(btn => btn.addEventListener('click', () => clickButton(btn)));
-buttonsOperations.forEach(btn => btn.addEventListener('click', () => clickButton(btn)));
+buttonsNumpad.forEach(btn => btn.addEventListener('click', () => clickNumber(btn)));
+buttonsOperations.forEach(btn => btn.addEventListener('click', () => clickOperator(btn)));
 buttonEqual.addEventListener('click', () => displayMain.value = solve(displayCalc.value));
 
 // functions
-
-// 
-function clickButton(btn) {
+/**
+ * Called when user clicks a number button (0-9 and '.').
+ * @param {HTML button node} btn 
+ */
+function clickNumber(btn) {
+    if (resultDelivered) {
+        resultDelivered = false;
+        displayCalc.value = '';
+    }
     displayCalc.value = displayCalc.value + btn.textContent;
 }
 
-function collapseBrackets(equation) {
-    // if no brackets, return simple arithmetic on equation
-    if (equation.findIndex(element => element === '(') === -1) {
-        return solve(equation);
+/**
+ * Called when user clicks an operation button (^, *, /, +, -).
+ * @param {HTML button node} btn 
+ */
+function clickOperator(btn) {
+    if (resultDelivered) {
+        resultDelivered = false;
+        displayCalc.value = displayMain.value;
     }
-
-    // if there are brackets, recursively solve each bracket, then return the simple arithmetic
-    let parsed = false;
-    while (!parsed) {
-        let nestedParen = 0;
-        let begin = false; // denotes a bracketed expression has been found and is being collapsed
-        let startIndex, endIndex;
-        for (let i = 0; i < equation.length; i++) {
-            if (equation[i] === '(') {
-                nestedParen++;
-                startIndex = i;
-                begin = true;
-            } else if (equation[i] === ')') {
-                nestedParen--;
-            }
-            if (nestedParen === 0 && begin) {
-                begin = false;
-                endIndex = i;
-                let collapsed = collapseBrackets(equation.slice(startIndex + 1, endIndex));
-                equation.splice(startIndex, endIndex - startIndex + 1, ...collapsed.toString().split(''));
-                i = 0;
-            }
-        }
-        parsed = true;
-    }
-    return equation;
+    displayCalc.value = displayCalc.value + btn.textContent;
 }
 
-// function collapseMultiplications(equation) {
+/**
+ * This function is called by solve(), and is used to collapse sub-problems into results, used to enforce order of operations (PEDMAS).
+ * @param {String} equation 
+ * @param {1-char String OR 2-length array of 1-char Strings} operators 
+ * @returns an equation with the sub-problems, involving the specified operators, solved.
+ */
+function collapse(equation, operators) {
 
-//     const FIND_OPERATORS_REGEX = /[*]/;
-//     const FIND_NUMBERS_REGEX = /\d*\.?\d+/;
-
-//     // collapse multiplications
-
-//     console.log('equation is: ' + equation);
-//     let index = equation.findIndex((element) => element === '*');
-//     while (index != -1) {
-//         console.log('index is: ' + index);
-//         let startIndex, endIndex;
-//         for (let i = index-1; i >= -1; i--) {
-//             if (i === -1) {
-//                 startIndex = 0;
-//                 break;
-//             } else if (equation[i].match(FIND_NUMBERS_REGEX) === null && equation[i] !== '.') {
-//                 startIndex = i;
-//                 break;
-//             }
-//         }
-//         for (let i = index+1; i <= equation.length; i++) {
-//             if (i === equation.length) {
-//                 endIndex = equation.length -1;
-//                 break;
-//             } else if (equation[i].match(FIND_NUMBERS_REGEX && equation[i] !== '.') === null) {
-//                 endIndex = i;
-//                 break;
-//             }
-//         }
-//         let slice = equation.slice(startIndex, endIndex+1);
-//         let result = arithmetic(slice.join('')); // currently causes stack overflow
-//         equation.splice(startIndex, endIndex-startIndex+1, result);
-//         index = equation.findIndex((element) => element === '*');
-//     }
-
-//     return equation;
-// }
-
-function collapse(equation, operator) {
-
-    //'/['+operator+']/'
-    const FIND_OPERATORS_REGEX = new RegExp('[\\' + operator + ']');
+    // build find operators regex
+    let regex;
+    if (operators instanceof Array) {
+        regex = new RegExp('[\\' + operators[0] + ']|[\\' + operators[1] + ']');
+    } else {
+        regex = new RegExp('[\\' + operators + ']');
+    }
+    const FIND_OPERATORS_REGEX = regex;
     const FIND_NUMBERS_REGEX = /\d*\.?\d+/;
 
-    // establish base case
+    // establish base case for recursion
     if (equation.join('').match(FIND_OPERATORS_REGEX) === null) {
         return equation;
     }
 
-    let index = equation.findIndex((element) => element === operator);
+    // find index of first operator
+    let index = equation.findIndex((element) => {
+        if (operators instanceof Array) {
+            return (element === operators[0] || element === operators[1]);
+        } else {
+            return element === operators;
+        }
+    });
+
+    // find start and end index of simple arithmetic and compute
     let startIndex, endIndex;
     for (let i = index - 1; i >= -1; i--) {
         if (i === -1) {
@@ -136,11 +101,22 @@ function collapse(equation, operator) {
     }
     let slice = equation.slice(startIndex, endIndex);
     let result = arithmetic(slice.join('')); // currently causes stack overflow
-    equation.splice(startIndex, endIndex - startIndex, ...result.toString().split(''));
-    return collapse(equation, operator);
+
+    // replace basic arithmetic with result
+    equation.splice(startIndex, endIndex - startIndex, ...result.split(''));
+
+    // recursive step
+    return collapse(equation, operators);
 }
 
+/**
+ * This function enforces order of operations on an equation. This means that first arithmetic in parenthesis are solved, then exponents, then
+ * multiplication and division from left to right, then addition and subtraction from left to right.
+ * @param {String} equation 
+ * @returns 
+ */
 function solve(equation) {
+
     // remove whitespace & conver to array of characters
     equation = equation.split('').filter(element => element !== ' ');
 
@@ -148,46 +124,54 @@ function solve(equation) {
     let operand = null;
     let operator = null;
 
-    // recursively solve bracketed sub-problems to enforce order of operations
+    //recursively solve bracketed sub-problems to enforce order of operations
     if (equation.lastIndexOf('(') != -1) {
         equation = collapseBrackets(equation);
     }
 
-    if (equation.lastIndexOf('*') != -1) {
-        equation = collapse(equation, operators.multiplication);
+    if (equation.lastIndexOf(operators.exponent) != -1) {
+        equation = collapse(equation, operators.exponent);
     }
 
-    if (equation.lastIndexOf('/') != -1) {
-        equation = collapse(equation, operators.division);
+    if (equation.lastIndexOf(operators.multiplication) != -1 || equation.lastIndexOf(operators.division) != -1) {
+        equation = collapse(equation, [operators.multiplication, operators.division]);
     }
 
-    if (equation.lastIndexOf('+') != -1) {
-        equation = collapse(equation, operators.addition);
+    if (equation.lastIndexOf(operators.addition) != -1 || equation.lastIndexOf(operators.subtraction) != -1) {
+        equation = collapse(equation, [operators.addition, operators.subtraction]);
     }
 
-    if (equation.lastIndexOf('+') != -1) {
-        equation = collapse(equation, operators.subtraction);
-    }
+    // delcare a result has been delivered
+    resultDelivered = true;
 
-    return equation.join('');
+    // 2 decimal places
+    let dp = parseFloat(equation.join(''));
+    dp = Math.round(dp*100)/100;
+
+    return dp.toString();
 }
 
+/**
+ * This function should only be called by solve(), which provides a simple problem that has only 2 operands and one operator. The equation must
+ * not contain whitespace, which is enforced by solve().
+ * 
+ * This function does not respect order of operations. 
+ * 
+ * @param {String} equation an equation represented in string format, for example, '1+1', '10/2' or '2^3'.
+ * @returns {String} the result of the equation.
+ */
 function arithmetic(equation) {
 
-    // function checkValidChars(equationArr) {
-    //     const VALID_CHARS = '0123456789+-*/^()';
-    //     for (let element of equationArr) {
-    //         console.log(VALID_CHARS.findIndex((e) => e === element));
-    //     }
-    // }
-
+    // define parts of problem
     let result = null;
     let operand = null;
     let operator = null;
 
+    // pure jank, refactor this for the love of dogs
     let equationStr = equation;
     equation = equation.split('');
 
+    // compute result
     const FIND_OPERATORS_REGEX = /[+]|[-]|[*]|[\/]|[\^]/;
     const FIND_NUMBERS_REGEX = /\d*\.?\d+/;
     for (let i = 0; i < equation.length; i++) {
@@ -234,24 +218,13 @@ function arithmetic(equation) {
         }
         operand = null;
     }
-    return result;
+    return result.toString();
 }
 
+/**
+ * Clears the two displays of the calculator
+ */
 function clear() {
     displayMain.value = ''
     displayCalc.value = '';
-    clearArr(operations);
 }
-
-function clearArr(arr) {
-    while (arr.length) {
-        arr.pop();
-    }
-}
-
-module.exports = {
-    solve,
-    arithmetic,
-    collapseBrackets,
-    collapse
-};
