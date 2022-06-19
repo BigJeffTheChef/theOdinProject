c68ebbad679b8f53f51ceef3078ff4c7eda91705
BigJeffTheChef <marleygpeter@gmail.com>
2022-06-19 17:50:07 +0100

    added to library project

--- full-stack-javascript/javascript/project-library/script.js
+++ full-stack-javascript/javascript/project-library/script.js
@@ -1,5 +1,48 @@
-const myLibrary = [];
+const library = [];
 
+// Page: Controls
+const addBookModalBtn = document.querySelectorAll('.add-book-button');
+addBookModalBtn.forEach(b => b.addEventListener('click', showModal));
+
+// Modal: Add Book
+const modalContainer = document.querySelector('.modal-container');
+const closeModalBtn = document.querySelector('#close-modal-button');
+const addToLibraryBtn = document.querySelector('#add-to-library');
+
+// Modal: Input Fields
+const fieldTitle = document.querySelector('#title');
+const fieldAuthor = document.querySelector('#author');
+const fieldPages = document.querySelector('#pages');
+const fieldRead = document.querySelector('#read');
+
+// Modal: Events
+closeModalBtn.addEventListener('click', () => hideModal());
+addToLibraryBtn.addEventListener('click', () => addToLibrary());
+
+// Modal: Functions
+function showModal() {
+    fieldTitle.value = "";
+    fieldAuthor.value = "";
+    fieldPages.value = "";
+    fieldRead.value = "";
+    modalContainer.classList.remove("hidden");
+}
+
+function hideModal() {
+    modalContainer.classList.add('hidden');
+    displayBooks();
+}
+
+function addToLibrary() {
+    const book = new Book(fieldTitle.value, fieldAuthor.value, fieldPages.value, fieldRead.checked);
+    library.push(book);
+    hideModal();
+}
+
+// Book Cards
+const bookCards = document.querySelector('.book-cards');
+
+// Book objects
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -7,32 +50,57 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
-function addBook(bookObj) {
-    if (bookObj instanceof Book) {
-        myLibrary.push(bookObj);
-    }
+function createCard(bookObj, index) {
+    let bookCard = document.createElement('div');
+
+    let bookCardHighlight = document.createElement('div');
+    let bookCardBody = document.createElement('div');
+    
+    let bookCardTitle = document.createElement('p');
+    let bookCardAuthor = document.createElement('p');
+    let bookCardPages = document.createElement('p');
+    let bookCardRemove = document.createElement('button');
+
+    bookCard.classList.add('book-card');
+    bookCardHighlight.classList.add('book-card-highlight');
+    bookCardHighlight.classList.add((bookObj.read) ? "read" : "unread");
+    bookCardBody.classList.add('book-card-body');
+    bookCardTitle.textContent = bookObj.title;
+    bookCardTitle.id = "card-title";
+    bookCardAuthor.textContent = bookObj.author;
+    bookCardAuthor.id = "card-author";
+    bookCardPages.textContent = bookObj.pages;
+    bookCardPages.id = "card-pages";
+    bookCardRemove.textContent = "X";
+    bookCardRemove.id = "card-remove";
+    bookCardRemove.addEventListener('click', () => {
+        library.splice(index, 1);
+        displayBooks();
+    });
+
+    bookCardBody.append(bookCardTitle, bookCardAuthor, bookCardPages, bookCardRemove);
+    bookCard.append(bookCardHighlight, bookCardBody);
+    bookCard.setAttribute("data-index", index);
+    return bookCard;
 }
 
 function displayBooks() {
-    for (let book of myLibrary) {
-        console.log(book);
+    // remove old bookCards
+    for (let i = bookCards.childNodes.length - 1; i >= 0;i--) {
+        bookCards.removeChild(bookCards.childNodes[i]);
+    }
+
+    // build new cards from library
+    for (let i = 0; i < library.length; i++) {
+        let card = createCard(library[i], i);
+        // card.setAttribute("data-index", i);
+        console.log("data-index: " + card.getAttribute("data-index"));
+        bookCards.appendChild(card);
     }
 }
 
-console.log('hi');
 
-const addBookBtn = document.querySelectorAll('.add-book-button');
-const modal = document.querySelector('.modal');
-const closeModalBtn = document.querySelector('#close-modal');
 
-//addBookBtn.addEventListener('click', () => modalToggle());
-addBookBtn.forEach(b => (
-    b.addEventListener('click', () => modalToggle())
-));
 
-closeModalBtn.addEventListener('click', () => modalToggle());
 
-function modalToggle() {
-    // addBookModal.classList.toggle('hidden');
-    modal.classList.toggle('hidden');
-}
+
