7be1007ed50ad82b0a82502dbf77b145948c8619
BigJeffTheChef <marleygpeter@gmail.com>
2022-06-25 18:40:01 +0100

    split css into modules webpack can combine, making general layout

--- full-stack-javascript/javascript/project-todo-list/src/index.js
+++ full-stack-javascript/javascript/project-todo-list/src/index.js
@@ -1,4 +1,7 @@
-import testyarentwe from './dom.js';
-import './styles/global.css';
-import './styles/containers.css';
+import './styles/global.css';   // global vars, fonts, resets
+import './styles/main.css';     // main container
+import './styles/header.css';   // header container
+import './styles/body.css';     // body container
+import './styles/nav.css';      // nav container
+import './styles/content.css';  // content container
 
