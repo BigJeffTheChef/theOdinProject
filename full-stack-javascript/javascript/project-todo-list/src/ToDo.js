b424cac4ef3c4eb7d8aacb49565c6500051994b9
BigJeffTheChef <marleygpeter@gmail.com>
2022-06-26 22:14:43 +0100

    begun to play around with localStorage

--- full-stack-javascript/javascript/project-todo-list/src/ToDo.js
+++ full-stack-javascript/javascript/project-todo-list/src/ToDo.js
@@ -117,74 +117,74 @@ class ToDoCheckList extends ToDo {
 
 }
 
-console.group('ToDo testing');
-let testObj = new ToDo('test title');
-console.log(testObj.toString());
-let testObj2 = new ToDo('test title 2');
-console.log(testObj2.toString());
-let testObj3 = new ToDo();
-console.log(testObj3.toString());
-testObj3.title = "dogs n stuff";
-console.log(testObj3.toString());
-console.log(ToDo.MAX_PRIORITY);
-console.log(testObj3);
-console.log(testObj3.constructor.PRIORITIES);   // can access static properties
-// from instance constructor
-console.log(testObj3.title);
-try {
-    testObj3.title = "     ";
-} catch (err) {
-    console.log(err);
-}
-console.log(testObj3.title);
-console.groupEnd('ToDo testing');
+// console.group('ToDo testing');
+// let testObj = new ToDo('test title');
+// console.log(testObj.toString());
+// let testObj2 = new ToDo('test title 2');
+// console.log(testObj2.toString());
+// let testObj3 = new ToDo();
+// console.log(testObj3.toString());
+// testObj3.title = "dogs n stuff";
+// console.log(testObj3.toString());
+// console.log(ToDo.MAX_PRIORITY);
+// console.log(testObj3);
+// console.log(testObj3.constructor.PRIORITIES);   // can access static properties
+// // from instance constructor
+// console.log(testObj3.title);
+// try {
+//     testObj3.title = "     ";
+// } catch (err) {
+//     console.log(err);
+// }
+// console.log(testObj3.title);
+// console.groupEnd('ToDo testing');
 
-console.group('ToDoChecklist testing');
+// console.group('ToDoChecklist testing');
 
-let c1 = new ToDoCheckList('test checklist', 'test description', 'some day', 1);
+// let c1 = new ToDoCheckList('test checklist', 'test description', 'some day', 1);
 
-console.log(c1.toString());
+// console.log(c1.toString());
 
-c1.addItem('a new list item', false);
+// c1.addItem('a new list item', false);
 
-console.log(c1.toString());
+// console.log(c1.toString());
 
-c1.addItem('another new list item', true);
-console.log(c1.toString());
+// c1.addItem('another new list item', true);
+// console.log(c1.toString());
 
-try {
-    c1.addItem('', true);
-} catch (err) {
-    console.log(err);
-}
+// try {
+//     c1.addItem('', true);
+// } catch (err) {
+//     console.log(err);
+// }
 
-try {
-    c1.addItem('    \n', true);
-} catch (err) {
-    console.log(err);
-}
+// try {
+//     c1.addItem('    \n', true);
+// } catch (err) {
+//     console.log(err);
+// }
 
-try {
-    c1.addItem('                      ', true);
-} catch (err) {
-    console.log(err);
-}
+// try {
+//     c1.addItem('                      ', true);
+// } catch (err) {
+//     console.log(err);
+// }
 
-try {
-    c1.addItem('x', 'yo');
-} catch (err) {
-    console.log(err);
-}
+// try {
+//     c1.addItem('x', 'yo');
+// } catch (err) {
+//     console.log(err);
+// }
 
-try {
-    c1.addItem('x', null);
-} catch (err) {
-    console.log(err);
-}
+// try {
+//     c1.addItem('x', null);
+// } catch (err) {
+//     console.log(err);
+// }
 
-try {
-    c1.addItem('x', undefined);
-} catch (err) {
-    console.log(err);
-}
-console.groupEnd('ToDoChecklist testing');
+// try {
+//     c1.addItem('x', undefined);
+// } catch (err) {
+//     console.log(err);
+// }
+// console.groupEnd('ToDoChecklist testing');
