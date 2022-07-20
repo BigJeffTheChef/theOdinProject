a394d9fcd003c7d2dfceb67facfee6d780051027
Peter Marley <pmarley@nanthealth.com>
2022-07-20 15:22:48 +0100

    added an ignored keyvault that hides api key for openweatheramp@

--- full-stack-javascript/javascript/apis/script.js
+++ full-stack-javascript/javascript/apis/script.js
@@ -1,6 +1,8 @@
-fetch('http://api.openweathermap.org/data/2.5/weather?q=Belfast&appid=a0d46b0ad51d9b6570c056c3a3a96f8c')
+import { OpenWeatherMapAPIKey } from '../keyvault/keyvault.js';
+
+fetch(`http://api.openweathermap.org/data/2.5/weather?q=Belfast&appid=${OpenWeatherMapAPIKey}`)
   .then(function (response) {
-    console.log(response.get);
+    console.log(response);
   }).catch(function (err) {
     console.log(`a wild error has appeared: ${err}`);
   })
