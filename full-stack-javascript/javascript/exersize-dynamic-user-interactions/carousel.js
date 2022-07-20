32ea8957c821f81335e0c3d2829eeacea341a10e
BigJeffTheChef <marleygpeter@gmail.com>
2022-07-21 00:53:58 +0100

    shite inefficient implementation of slideTo

--- full-stack-javascript/javascript/exersize-dynamic-user-interactions/carousel.js
+++ full-stack-javascript/javascript/exersize-dynamic-user-interactions/carousel.js
@@ -6,6 +6,8 @@ window.onload = function initialise() {
         view: document.querySelector('.carousel>.view'),
         hud: document.querySelector('.carousel>.hud'),
         controls: document.querySelector('.carousel>.controls'),
+        previousBtn: document.querySelector('.carousel>.controls>div:first-child'),
+        nextBtn: document.querySelector('.carousel>.controls>div:last-child'),
     };
 
     ((function configureCarousel() {
@@ -20,12 +22,13 @@ window.onload = function initialise() {
             'i8.jpg',
         ];
         let displayFirst = true;
-        for (let img of imgs) {
+
+        for (let i = 0; i < imgs.length; i++) {
             let imgElement = document.createElement('img');
             let imgContainer = document.createElement('div');
             let dot = document.createElement('div');
 
-            imgElement.src = img;
+            imgElement.src = imgs[i];
             dot.textContent = 'o';
 
             imgContainer.classList.add('carousel-image');
@@ -35,23 +38,70 @@ window.onload = function initialise() {
                 dot.classList.add('current');
             } else {
                 imgContainer.classList.add('off-right');
-                dot.classList.add('.off-right')
-                dot.addEventListener('click', ()=> slide('next'));
+                dot.classList.add('off-right')
             }
+            dot.addEventListener('click', ()=> slideTo(i));
             imgContainer.appendChild(imgElement);
             elements.carousel.view.appendChild(imgContainer);
             elements.carousel.hud.appendChild(dot);
+
         }
-        document.querySelector('.carousel>.controls>div:first-child').addEventListener('click', () => slide('previous'));
-        document.querySelector('.carousel>.controls>div:last-child').addEventListener('click', () => slide('next'));
+        // document.querySelector('.carousel>.controls>div:first-child').addEventListener('click', () => slide('previous'));
+        if (imgs.length > 1) {
+            document.querySelector('.carousel>.controls>div:last-child').addEventListener('click', () => slideTo(1));
+        }
+        
     })());
 };
 
-// function slideTo(slideNumber) {
-//     const [dots] = document.querySelectorAll('.carousel>.hud>.div');
-//     console.log(dots[0]);
-//     console.log(dots[dots.length-1]);
-// }
+function slideTo(slideNumber) {
+    const dots = elements.carousel.hud.querySelectorAll('div');
+    const slides = elements.carousel.view.querySelectorAll('.carousel-image');
+
+    // const currentDot = elements.carousel.hud.querySelector('current');
+    // const prevDot = currentDot.previousElementSibling;
+    // const nextDot = currentDot.nextElementSibling;
+
+    for (let i = 0; i < dots.length && i < slides.length; i++) {
+        if (i < slideNumber) {
+            dots[i].classList.add('off-left');
+            dots[i].classList.remove('off-right');
+            dots[i].classList.remove('current');
+
+            slides[i].classList.add('off-left');
+            slides[i].classList.remove('off-right');
+            slides[i].classList.remove('current');
+        } else if (i > slideNumber) {
+            dots[i].classList.remove('off-left');
+            dots[i].classList.add('off-right');
+            dots[i].classList.remove('current');
+
+            slides[i].classList.remove('off-left');
+            slides[i].classList.add('off-right');
+            slides[i].classList.remove('current');
+        } else {
+            dots[i].classList.remove('off-left');
+            dots[i].classList.remove('off-right');
+            dots[i].classList.add('current');
+
+            slides[i].classList.remove('off-left');
+            slides[i].classList.remove('off-right');
+            slides[i].classList.add('current');
+        }
+    }
+
+    if (slideNumber - 1 > 0) {
+        elements.carousel.previousBtn.addEventListener('click', () => slideTo(slideNumber - 1));
+    } else {
+        elements.carousel.previousBtn.removeEventListener('click', () => {});
+    }
+
+    if (slideNumber + 1 < slides.length) {
+        elements.carousel.nextBtn.addEventListener('click', () => slideTo(slideNumber + 1));
+    } else {
+        elements.carousel.nextBtn.removeEventListener('click', () => {});
+    }
+}
 
 function slide(direction) {
     //if (direction !== 'next' && direction !== 'previous') throw Error(`changeSlide( direction ) received unrecognised direction parameter: \"${direction}\" - must be 'next' or 'previous' only.`);
