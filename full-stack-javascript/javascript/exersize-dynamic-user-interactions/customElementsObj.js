226fc3da1f9f956e62cfe9cbc5f3039da20e8c56
BigJeffTheChef <marleygpeter@gmail.com>
2022-07-22 01:40:14 +0100

    carousel working

---
+++ full-stack-javascript/javascript/exersize-dynamic-user-interactions/customElementsObj.js
@@ -0,0 +1,11 @@
+class PopUpInfo extends HTMLElement {
+    constructor() {
+      // Always call super first in constructor
+      super();
+  
+      // write element functionality in here
+  
+      //...
+    }
+  }
+  
