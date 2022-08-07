8f605fe8f410a74b3214f3a08bd80b6bad55983c
BigJeffTheChef <marleygpeter@gmail.com>
2022-08-07 11:17:24 +0100

    -

--- full-stack-javascript/javascript/exersize-recursion/fibonacci.js
+++ full-stack-javascript/javascript/exersize-recursion/fibonacci.js
@@ -1,42 +1,26 @@
 
-console.log(fibs(8));
-console.log(fibsArrayRec(4));
-
-function fibs(fibIndex) {
-  if (fibIndex === 0) throw new Error('cant have the 0th number of a sequence laddy.');
-  const nums = [0];
-  if (fibIndex === 1) return nums;
-  nums.push(1);
-  for (let i = 2; i < fibIndex; i++) {
-    nums.push(nums[i-1] + nums[i-2]);
-  }
-  return nums;
-}
-
-function fibsArrayRec(n) {
-  const seq = [];
-  for (let i = 0; i < n; i++) {
-    seq.push(fibsRec(i));
+function fibs(num) {
+  const seq = [0, 1];
+  if (num <= 2) return seq.slice(0, num);
+  for (let i = 2; i < num; i++) {
+    seq.push(seq[i - 2] + seq[i - 1]);
   }
   return seq;
 }
 
-function fibsRec(fibIndex) {
-  // base case
-  if (fibIndex <= 1) return 0;
-  if (fibIndex === 2) return 1;
+console.log(fibs(3));
 
-  const prev1 = fibsRec(fibIndex - 1);
-  const prev2 = fibsRec(fibIndex- 2 );
-  return prev1 + prev2;
-  const seq = [];
-  seq.push(prev2);
-  seq.push(prev1);
-  const next = prev1 + prev2;
-  
-  return next;
-
+function fibsRec(num) {
+  const seq = new Array(num - 1).fill(null);
+  for (let i = 1; i <= num; i++) seq[i - 1] = f(i);
+  return seq;
 }
 
-// base case: if fibIndex is <= 1 return that number;
-// otherwise, create an array
+
+function f(number) {
+  if (number <= 0) throw Error('cannot find zeroth number of fibonacci sequence');
+  return (number === 1 || number === 2) ? number - 1 : f(number - 2) + f(number - 1);
+}
+
+console.log(f(8));
+console.log(fibsRec(8));
