3b70593512c4b568062f8f7b840b13346401d0f3
BigJeffTheChef <marleygpeter@gmail.com>
2023-05-18 23:43:00 +0100

    memory card assignment

--- full-stack-javascript/javascript/react/secondRun/assignment2-memory-card/memory-card/src/App.js
+++ full-stack-javascript/javascript/react/secondRun/assignment2-memory-card/memory-card/src/App.js
@@ -1,28 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
 import ImageWindow from './components/ImageWindow';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+
+
 
 function App(props) {
+    // users current score (number of not previously chosen images selected)
+    const [score, setScore] = useState(0)
 
-  const [score, setScore] = useState(0)
+    // chosen is an array of image alt's "eg armadillo"
+    const [chosen, setChosen] = useState([]);
 
-  function handleScoreUpdate(newScore) {
-    setScore(newScore);
-  }
+    // boolean flag representing if the game is over (ie, a player has won)
+    const [gameOver, setGameOver] = useState(false);
 
+    function handleImageClick(event) {
+        const id = event.target.alt;
+        console.log(`id is ${id}`);
 
-  return (
-    <div className="App">
-      <div>{score}</div>
-      <ImageWindow
-        imgs={props.imgs}
-        imgCount={4}
-        score={score}
-        onScoreUpdate={handleScoreUpdate}
-      />
-    </div>
-  );
+        // if image has already been chosen, reset score
+        if (chosen.includes(id)) {
+            setChosen(prev => []);
+            setScore(0);
+            console.log("previously chosen - SCORE set to 0");
+            return;
+        }
+
+        setChosen(prev => [...prev, id]);
+        setScore(score + 1);
+        console.log("new image chosen - SCORE +1");
+    }
+
+    if (gameOver) {
+        return (<div>You won!</div>)
+    } else {
+        return (
+            <div className="App">
+                <div>Current Score: {score}</div>
+                <ImageWindow
+                    imgs={props.imgs}
+                    imgCount={4}
+                    chosen={chosen}
+                    handleImageClick={handleImageClick}
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
--- full-stack-javascript/javascript/react/secondRun/assignment2-memory-card/memory-card/src/components/ImageWindow.js
+++ full-stack-javascript/javascript/react/secondRun/assignment2-memory-card/memory-card/src/components/ImageWindow.js
@@ -1,113 +1,66 @@
-import { useEffect, useState, useCallback } from 'react';
+import {useEffect, useState, useCallback} from 'react';
 import '../styles/ImageWindow.css';
 import Image from './Image';
 
-
-
-
-
 function ImageWindow(props) {
     // destructure props
-    let { imgs, imgCount } = props;
-    if (!imgCount) {
-        imgCount = imgs.length;
-    }
-    const [chosen, setChosen] = useState([]);
-    const handleImageClick = useCallback((event) => {
-        console.log(`id ${event.target.id}`);
-        const id = Number(event.target.id);
-        console.log(`length was: ${chosen.length}`);
-        console.log(`score was: ${props.score}`);
+    const {imgs, imgCount, handleImageClick, chosen } = props;
 
-        // if image has already been chosen, reset score
-        if (chosen.includes(id)) {
-            setChosen(prev => []);
-            props.onScoreUpdate(0);
-            console.log("previously chosen - SCORE 0");
-            return;
-        }
-        setChosen(prev => [...prev, id]);
-        props.onScoreUpdate(prevScore => prevScore + 1);
-        // add new image not in starting images array
-        let components = imageComponents;
-
-        // select valid images to potentially be added
-        let selectableImages = imagesCreated.filter(e => !startingImgs.includes(e));
-
-        // must not be the chosen image, must not be in imageComponents
-        const removalIndex = components.findIndex(e => e.props.id == id)
-        components.splice(removalIndex, 1);
-
-        // add a random element from selectableImages to components at index of removed element
-        const rand = Math.random()
-        const index = Math.round(rand * selectableImages.length) - 1;
-        const randomSelection = selectableImages.splice(index, 1)[0];
-        components.splice(removalIndex, 0, randomSelection);
-        setImageComponents(prev => components);
-        // remove old image
-        // setImageComponents
-
-        console.log("new image chosen - SCORE +1");
-    }, [chosen, props]);
-    // configure component state and hooks
-    useEffect(() => {
-        console.log(`[score: ${props.score}] [chosen is now: ${JSON.stringify(chosen)}]`);
-    }, [props.score, chosen]);
+    // // validate imgCount
+    // if (!imgCount) {
+    //     imgCount = imgs.length;
+    // }
 
     // create images
     const imagesCreated = [];
     for (let i = 0; i < imgs.length; i++) {
-        const { src, alt } = imgs[i];
-        imagesCreated.push(<Image src={src} alt={alt} key={alt} id={i} onClick={handleImageClick} />)
+        const {src, alt} = imgs[i];
+        imagesCreated.push(
+            <Image
+                src={src}
+                alt={alt}
+                key={alt}
+                id={alt}
+                onClick={handleImageClick}
+            />
+        );
     }
-    const startingImgs = imagesCreated.slice(0, imgCount);
-    //const restOfImgs = imagesCreated.slice(imgCount);
-    const [imageComponents, setImageComponents] = useState(startingImgs);
+    //const imagesDisplayed = imagesCreated.slice(0, imgCount);
+    let i = 0;
 
-    // handlers
+    const imagesDisplayed = imagesCreated
+        .map(image => ({image, sortVal: Math.random()}))
+        .sort((a,b) => a.sortVal - b.sortVal)
+        .map(imageObj => imageObj.image)
+        .filter(image => i++ < imgCount);
 
-    // function handleImageClick(event) {
-    //     console.log(`id ${event.target.id}`);
-    //     const id = Number(event.target.id);
-    //     console.log(`length was: ${chosen.length}`);
-    //     console.log(`score was: ${props.score}`);
+    let inChosen = 0;
+    for (const imagesDisplayedElement of imagesDisplayed) {
+        if (chosen.includes(imagesDisplayedElement.props.alt)) {
+            inChosen++;
+        }
+    }
 
-    //     // if image has already been chosen, reset score
-    //     if (chosen.includes(id)) {
-    //         setChosen([]);
-    //         props.onScoreUpdate(0);
-    //         console.log("previously chosen - SCORE 0");
-    //         return;
-    //     }
-    //     setChosen(prev => [...prev, id]);
-    //     props.onScoreUpdate(prevScore => prevScore + 1);
-    //     // add new image not in starting images array
-    //     let components = imageComponents;
-
-    //     // select valid images to potentially be added
-    //     let selectableImages = imagesCreated.filter(e => !startingImgs.includes(e));
-
-    //     // must not be the chosen image, must not be in imageComponents
-    //     const removalIndex = components.findIndex(e => e.props.id == id)
-    //     components.splice(removalIndex, 1);
-
-    //     // add a random element from selectableImages to components at index of removed element
-    //     const rand = Math.random()
-    //     const index = Math.round(rand * selectableImages.length) - 1;
-    //     const randomSelection = selectableImages.splice(index, 1)[0];
-    //     components.splice(removalIndex, 0, randomSelection);
-    //     setImageComponents(prev => components);
-    //     // remove old image
-    //     // setImageComponents
-
-    //     console.log("new image chosen - SCORE +1");
-    // }
+    if (inChosen === imgCount) {
+        const randomIndex = Math.round(Math.random() * imgCount) - 1;
+        const mapped = imagesDisplayed.map(i => i.props.alt);
+        // const newImgTag = chosen.find(altTag => {
+        //     const includes = !mapped.includes(altTag)
+        //     return includes;
+        // });
+        const newImg = imagesCreated.find(image => {
+            const includes = !mapped.includes(image.props.alt)
+            return includes;
+        });
+        //const newImg = imagesCreated.find(i => i.props.alt === newImgTag)
+        imagesDisplayed.splice(randomIndex, 1, newImg);
+    }
 
     // return component
     return (
         <div className='ImageWindowContainer'>
             <div className='ImageWindow'>
-                {imageComponents}
+                {imagesDisplayed}
             </div>
         </div>
     );
